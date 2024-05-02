a4e357e0aa11068a769d54e4e0bd54e1a593e8b4
Gianluca <gianluca.maragliano@usi.ch>
2024-05-02 14:38:09 +0200

    XSS DETECTION MODEL: Refined Preprocessing

--- src/detection_models/utils/general.py
+++ src/detection_models/utils/general.py
@@ -1,18 +1,25 @@
+import numpy as np
+import pandas as pd
+
 from src.utils.preprocessor import preprocess_payload
 from src.utils.tokenizer import xss_tokenizer, clean_tokenized_payloads
 from src.utils.vectorizer import xss_payloads_vectorizer, get_sorted_tokens
 
 
-def process_payloads(payloads):
+def process_payloads(payloads, sorted_tokens=None):
     # Preprocess payloads
     preprocessed_payloads = preprocess_payload(payloads['Payloads'])
+
     # Tokenize payloads
     tokenized_payloads = [xss_tokenizer(payload) for payload in preprocessed_payloads]
+
     # Vectorize payloads
-    x_tfidf = xss_payloads_vectorizer(preprocessed_payloads, tokenized_payloads)
-    sorted_tokens = get_sorted_tokens(x_tfidf)
+    if sorted_tokens is None:
+        x_tfidf = xss_payloads_vectorizer(preprocessed_payloads, tokenized_payloads)
+        sorted_tokens = get_sorted_tokens(x_tfidf)
+        sorted_tokens.append('None')
+
     cleaned_tokenized_payloads = clean_tokenized_payloads(tokenized_payloads, sorted_tokens)
-    sorted_tokens.append('None')
     return sorted_tokens, cleaned_tokenized_payloads
 
 
